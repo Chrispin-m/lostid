1a171bff748a4c989210f7cecc9b3198fb4497de
Chrispin-m <mwangi.wachira2019@students.jkuat.ac.ke>
2024-12-04 20:20:02 +0300

    sqlite

--- lost_id_recovery/settings.py
+++ lost_id_recovery/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'lost_id_recovery.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -112,7 +112,7 @@ DATABASES = {
 }
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
-
+'''
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
