ac311d0f587901a79888b2d3949487b3dcee55a1
Chrispin-m <mwangi.wachira2019@students.jkuat.ac.ke>
2024-12-04 14:37:34 +0300

    renderyaml

--- lost_id_recovery/urls.py
+++ lost_id_recovery/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('0703175863/admin/', admin.site.urls),
     path('api/ids/', include('ids.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
