c250eefb47eb24bc9f169e639976d7adb755d9df
Chrispin-m <mwangi.wachira2019@students.jkuat.ac.ke>
2024-12-04 18:55:14 +0300

    signal

--- ids/utils.py
+++ ids/utils.py
@@ -4,7 +4,6 @@ from PIL import Image, ImageEnhance, ImageFilter
 def extract_text_from_image(image):
     if not isinstance(image, Image.Image):
         image = Image.open(image)
-
     image = image.convert("L")  
     image = image.filter(ImageFilter.MedianFilter())  
     enhancer = ImageEnhance.Contrast(image)
