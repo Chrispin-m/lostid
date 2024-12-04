98285755960079a88dc453d30fd2d89ab5656a76
Chrispin-m <mwangi.wachira2019@students.jkuat.ac.ke>
2024-12-04 18:49:41 +0300

    signal

--- ids/utils.py
+++ ids/utils.py
@@ -1,19 +1,18 @@
 import pytesseract
 from PIL import Image, ImageEnhance, ImageFilter
 
-def extract_text_from_image(image_path):
-    # Open the image
-    image = Image.open(image_path)
-    
-    # Optional preprocessing for better OCR results
-    image = image.convert("L")  # Convert to grayscale
-    image = image.filter(ImageFilter.MedianFilter())  # Reduce noise
+def extract_text_from_image(image):
+    if not isinstance(image, Image.Image):
+        image = Image.open(image)
+
+    image = image.convert("L")  
+    image = image.filter(ImageFilter.MedianFilter())  
     enhancer = ImageEnhance.Contrast(image)
-    image = enhancer.enhance(2)  # Increase contrast
-    
+    image = enhancer.enhance(2)  
+
     # Extract text using pytesseract
-    custom_config = r'--psm 6'  # Page segmentation mode: Assume a single uniform block of text
+    custom_config = r'--psm 6' 
     text = pytesseract.image_to_string(image, config=custom_config)
     print(text)
-    
+
     return text
