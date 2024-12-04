66edbe05a8a60bcd75e8bf3faaf439171aaa7c69
Chrispin-m <mwangi.wachira2019@students.jkuat.ac.ke>
2024-12-04 14:59:34 +0300

    bashdtvenv

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
-sudo apt-get install -y tesseract-ocr -y
+if [ ! -d "venv" ]; then
+  python3 -m venv venv
+fi
+source venv/bin/activate
+apt-get install -y tesseract-ocr -y python3-venv python3-pip
 pip install -r requirements.txt
