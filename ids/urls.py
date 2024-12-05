9c497ebfd1f12c7ca85e738b78ee7bf1d300f910
Chrispin-m <mwangi.wachira2019@students.jkuat.ac.ke>
2024-12-05 16:58:31 +0300

    unecelink

--- ids/urls.py
+++ ids/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.FoundIDListView.as_view(), name='found_id_list'),
-    path('<uuid:id>/', views.FoundIDDetailView.as_view(), name='found_id_detail'),
     path('post/', views.PostFoundID.as_view(), name='post_found_id'),
     path('search/', views.SearchLostID.as_view(), name='search_lost_id'),
     path('<uuid:id>/messages/', views.FoundIDMessagesView.as_view(), name='found_id_messages'),
